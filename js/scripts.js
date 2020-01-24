cbc215cdd239920c362c98d3b100e407878eda2c
Prestwick97 <prestwick97@gmail.com>
2020-01-24 12:02:06 -0800

    add number response logic for '1', '2', and '3'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
   //Business Logic
+  // var output = ("#output").text();
+  var three = "I'm sorry, Dave. I'm afraid I can't do that."
+  var two = "Boop!"
+  var one = "Beep!"
   var inputNum = ("input#inputNum").val();
-  for(var i = 0; inputNum.length(); i++){
-    
+  for(var i = 0; inputNum.length(); i++) {
+    if (i==3) {
+console.log(three);
+    }
+    else if (i ===2) {
+      console.log(two);
+    }
+    else if (i===1) {
+      console.log(one);
+    }
+    else {
+      console.log(i);
+    }
   }
 
   //Interface Logic
   $("form#formOne").submit(function(event){
     event.preventDefault();
-
   })
 });
