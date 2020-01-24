e0f4feea44b084e70467f6f3ca453647b04e1f5c
Prestwick97 <prestwick97@gmail.com>
2020-01-24 13:19:22 -0800

    turn index into a string for legible output

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
 $(document).ready(function(){
-  //Business Logic
-  var output = ("#output").text();
-  var three = "I'm sorry, Dave. I'm afraid I can't do that."
-  var two = "Boop!"
-  var one = "Beep!"
-  var input = $("input#inputNum").val();
-  var inputNum = input.split();
-  console.log(inputNum);
-  for(var i = 0; inputNum.length; i++) {
-    if (i==3) {
-console.log(three);
-    }
-    else if (i ===2) {
-      console.log(two);
-    }
-    else if (i===1) {
-      console.log(one);
-    }
-    else {
-      // console.log(inputNum.length);
+  var HAL = function(number){
+    var output = [];
+    for(var i = 0; i <= number; i++){
+      var numOut = i.toString()
     }
   }
+
+
+
+
+
+
+
+
+
+  //   //Business Logic
+//   var output = ("#output").text();
+//   var three = "I'm sorry, Dave. I'm afraid I can't do that."
+//   var two = "Boop!"
+//   var one = "Beep!"
+//   var input = $("input#inputNum").val();
+//   var inputNum = input.split();
+//   console.log(inputNum);
+//   for(var i = 0; inputNum.length; i++) {
+//     if (i==3) {
+// console.log(three);
+//     }
+//     else if (i ===2) {
+//       console.log(two);
+//     }
+//     else if (i===1) {
+//       console.log(one);
+//     }
+//     else {
+//       // console.log(inputNum.length);
+//     }
+//   }
 
   //Interface Logic
   $("form#formOne").submit(function(event){
