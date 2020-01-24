ef1a0df3cc0a4fc182f00e7877a6165d5ff00ff2
Prestwick97 <prestwick97@gmail.com>
2020-01-24 11:53:40 -0800

    add 'output' div in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-  
+  //Business Logic
+  var inputNum = ("input#inputNum").val();
+  for(var i = 0; inputNum.length(); i++){
+    
+  }
 
+  //Interface Logic
   $("form#formOne").submit(function(event){
     event.preventDefault();
 
