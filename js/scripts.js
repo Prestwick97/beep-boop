225093d6257093183cda6c896be846c55430304a
Prestwick97 <prestwick97@gmail.com>
2020-01-26 14:57:11 -0800

    remove unneccessary splitting of input

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
   var one = "Beep!"
   var two = "Boop!"
   var three = "I'm sorry, Dave. I'm afraid I can't do that."
+
   var HAL = function(number){
     if(number == 3){
       console.log(three);
@@ -12,11 +13,10 @@ $(document).ready(function(){
     else if (number == 2){
       console.log(two)
     }
-    else if (number==1){
+    else if (number == 1){
       console.log(one);
     }
     else {
-      number.split("")
       console.log(number);
     }
   }
