0c4ecd5883eeb87ac5f90d54c12906f40e17c937
Prestwick97 <prestwick97@gmail.com>
2020-01-26 14:35:27 -0800

    comment out all logic code - keep getting jquery error, restarting from scratch again

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       if (numOut === 3) {
         output.push(three);
       }
-      else if(numOut === 2) { //Can't figure out why else if statements aren't working
+      else if(numOut === 2) {
         output.push(two);
         var two = "Boop!"
       }
@@ -20,20 +20,20 @@ $(document).ready(function(){
         output.push(i)
       };
       console.log(output);
-      //return(output);
     }
    
   };
 
-  console.log(HAL(output));
 
+HAL(input);
 
 
 var input = $("input#inputNum").val
   //Interface Logic
   $("form#formOne").submit(function(event){
     event.preventDefault();
-  $("#resultOutput").text(HAL(input));
+    console.log(input);
+  // $("#resultOutput").text(input);
 
   });
 });
