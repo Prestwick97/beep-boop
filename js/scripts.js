fe77a02d707b0e431324bcd27c0df2cd0ef4c873
Prestwick97 <prestwick97@gmail.com>
2020-01-25 21:44:30 -0800

    FIX else if statements by deleting unneccessary semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,25 @@ $(document).ready(function(){
   var HAL = function(number){
     var output = []; 
     var three = "I'm sorry, Dave. I'm afraid I can't do that."
-    for(var i = 0; i <= number; i++){
+    for (var i = 0; i <= number; i++){
       var numOut = i.toString();
       if (numOut === 3) {
         output.push(three);
-      };
+      }
       else if(numOut === 2) { //Can't figure out why else if statements aren't working
         output.push(two);
         var two = "Boop!"
-      };
+      }
       else if(numOut === 1) {
         var one = "Beep!"
         output.push(one);
-      };
+      }
       else {
         output.push(i)
-      }
+      };
       console.log(output);
       //return(output);
-    });
+    }
    
   };
 
