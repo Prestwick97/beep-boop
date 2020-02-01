e03e661e2fb7aa28c54cc9184554dba8f7bb3dbe
Prestwick97 <prestwick97@gmail.com>
2020-02-01 15:58:22 -0800

    split input into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
   //business logic
   var input = parseInt(prompt("Please enter a number:"));
-  // var inputArray = input.split("");
+  var inputArr = input.split("");
   var output = [];
-  var one = "Beep!"
-  var two = "Boop!"
-  var three = "I'm sorry, Dave. I'm afraid I can't do that."
+  var one = "Beep!";
+  var two = "Boop!";
+  var three = "I'm sorry, Dave. I'm afraid I can't do that.";
 
+  // var HAL = function(number){
+  //   for(var i = 0; i <=number.length; i++){
+  //     output.push(i);
+  //     if(number[i].includes(1)) {
+  //       output.push("beep!")
+  //     }
+  //   }
+  // }
   var HAL = function(number){
-    for(var i = 0; i<=number; i++){
-output.push(i);
+    for (var i = 1; i <= number; ++i ){
+      if(i == 3){
+        output.push(three);
+      }
+      else if (i == 2){
+        output.push(two);
+      }
+      else if (i == 1){
+        output.push(one);
+      }
+      else {
+        output.push(input);
+      }
     }
   }
-
 
   HAL(input);
   console.log(output);
