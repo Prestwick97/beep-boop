be68270513619b3014abcd7bcbc3b66f098e9b29
Prestwick97 <prestwick97@gmail.com>
2020-02-01 13:21:44 -0800

    retart logic, create initial for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,19 @@
 $(document).ready(function(){
   //business logic
-  var input = prompt("Please enter a number:").split("");
+  var input = parseInt(prompt("Please enter a number:"));
+  // var inputArray = input.split("");
   var output = [];
   var one = "Beep!"
   var two = "Boop!"
   var three = "I'm sorry, Dave. I'm afraid I can't do that."
 
   var HAL = function(number){
-    for (var i = 1; i <= number; ++i ){
-      if(number == 3){
-        output.push(three);
-      }
-      else if (number == 2){
-        output.push(two);
-      }
-      else if (number == 1){
-        output.push(one);
-      }
-      else {
-        output.push(input);
-      }
+    for(var i = 0; i<=number; i++){
+output.push(i);
     }
   }
+
+
   HAL(input);
   console.log(output);
 
