e2762f97968c8c412c621745a7b2e58c00e0d164
Prestwick97 <prestwick97@gmail.com>
2020-02-03 12:45:20 -0800

    update variable for index string

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,16 @@ $(document).ready(function(){
   var input = prompt("Please enter a number:").split("");
   var inputNum = parseInt(input);
   var output = [];
-  // var HAL = function(number){
-  //   for(var i = 0; i <=number.length; i++){
-  //     output.push(i);
-  //     if(number[i].includes(1)) {
-  //       output.push("beep!")
-  //     }
-  //   }
-  // }
   var HAL = function(number){
     for(var i = 1; i <= number; i++ ){
-      if(i.tostring.includes("3")){
+      var str = i.toString();
+      if(str.includes("3")){
         output.push("I'm sorry, Dave. I'm afraid I can't do that.");
       }
-      else if(i.tostring.includes("2")){
+      else if(str.includes("2")){
         output.push("Boop!");
       }
-      else if(i.tostring.includes("1")){
+      else if(str.includes("1")){
         output.push("Beep!");
       }
       else {
