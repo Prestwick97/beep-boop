a7c44ac6adb926d6bd21c56d14c2278a66334268
Prestwick97 <prestwick97@gmail.com>
2020-02-02 20:56:42 -0800

    replace variables with string in if statements

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,6 @@ $(document).ready(function(){
   var input = prompt("Please enter a number:").split("");
   var inputNum = parseInt(input);
   var output = [];
-  var one = "Beep!";
-  var two = "Boop!";
-  var three = "I'm sorry, Dave. I'm afraid I can't do that.";
-
   // var HAL = function(number){
   //   for(var i = 0; i <=number.length; i++){
   //     output.push(i);
@@ -16,15 +12,15 @@ $(document).ready(function(){
   //   }
   // }
   var HAL = function(number){
-    for (var i = 1; i <= number; i++ ){
+    for(var i = 1; i <= number; i++ ){
       if(i === 3){
-        output.push(three);
+        output.push("I'm sorry, Dave. I'm afraid I can't do that.");
       }
-      else if (i === 2){
-        output.push(two);
+      else if(i === 2){
+        output.push("Boop!");
       }
-      else if (i === 1){
-        output.push(one);
+      else if(i === 1){
+        output.push("Beep!");
       }
       else {
         output.push(i);
