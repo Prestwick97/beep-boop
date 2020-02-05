8fa4898d8166d0c9d03d101fdcee1fc877d41c8f
Prestwick97 <prestwick97@gmail.com>
2020-02-04 20:39:08 -0800

    change string approach to adjust functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,44 @@
 $(document).ready(function(){
   //business logic
-  var input = prompt("Please enter a number:").split("");
-  var inputNum = parseInt(input);
   var output = [];
   var HAL = function(number){
-    for(var i = 1; i <= number; i++ ){
-      var str = i.toString();
-      if(str.includes("3")){
-        output.push("I'm sorry, Dave. I'm afraid I can't do that.");
-      }
-      else if(str.includes("2")){
-        output.push("Boop!");
-      }
-      else if(str.includes("1")){
-        output.push("Beep!");
-      }
-      else {
-        output.push(i);
+    var str = number.toString();
+
+    if(str.includes(3)){
+      output.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+    }
+    else if(str.includes(2)){
+      output.push(" Boop!");
+    }
+    else if(str.includes(1)){
+      output.push(" Beep!");
+    }
+    else {
+      for(var i = 1; i <= number; ++i){
+        var str = i.toString();
+        if(str.includes(3)){
+          output.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+        }
+        else if(str.includes(2)){
+          output.push(" Boop!");
+        }
+        else if(str.includes(1)){
+          output.push(" Beep!");
+        }
+        else{
+        output.push(i)
+        }
       }
     }
   }
+  // User Interface
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+    console.log(inputNum);
+    var input = $("#inputNum").val().split(" ");
+    var inputNum = parseInt(input);
 
-  HAL(inputNum);
-  console.log(output);
-
+    HAL(inputNum);
+    $("#resultOutput").text(output);
+  });
 });
