cfeea11011678f88b1d35d5dc08586d5c5cfeff0
Student <example@example.com>
2020-02-06 15:50:28 -0800

    remove unneccessary loop, seperate business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-$(document).ready(function(){
-  //business logic
-  var output = [];
-  var HAL = function(number){
-    var str = number.toString();
-
+//business logic
+var output = [];
+var HAL = function(number){
+  for(var i = 1; i <= number; ++i){
+    var str = i.toString();
     if(str.includes(3)){
       output.push(" I'm sorry, Dave. I'm afraid I can't do that.");
     }
@@ -13,31 +12,18 @@ $(document).ready(function(){
     else if(str.includes(1)){
       output.push(" Beep!");
     }
-    else {
-      for(var i = 1; i <= number; ++i){
-        var str = i.toString();
-        if(str.includes(3)){
-          output.push(" I'm sorry, Dave. I'm afraid I can't do that.");
-        }
-        else if(str.includes(2)){
-          output.push(" Boop!");
-        }
-        else if(str.includes(1)){
-          output.push(" Beep!");
-        }
-        else{
-        output.push(" " + i)
-        }
-      }
+    else{
+      output.push(" " + i)
     }
-  }
-  // User Interface
+  } 
+}
+
+// User Interface
+$(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
-    console.log(inputNum);
     var input = $("#inputNum").val().split(" ");
     var inputNum = parseInt(input);
-
     HAL(inputNum);
     $("#resultOutput").text(output);
   });
